cdac09e89386367606d789954056c9a44df558e5
biboon <r.libaert@gmail.com>
2015-05-07 16:29:53 +0200

    cleaned slip_configure script

--- Scripts/slip_configure.sh
+++ Scripts/slip_configure.sh
@@ -2,7 +2,6 @@
 
 slattach -L -s 9600 -p slip /dev/ttyUSB0 &
 sleep 1
-#ifconfig sl0 192.168.0.1 dstaddr 192.168.1.4 mtu 1500
 ip link set sl0 up
 ip address add dev sl0 172.26.79.214 peer 172.26.79.215
 echo 1 > /proc/sys/net/ipv4/ip_forward
